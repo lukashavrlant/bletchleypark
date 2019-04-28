f599aabc63362abd448aa495814d0df91bcd3199
Lukáš Havrlant <lukas@havrlant.cz>
2019-04-28 18:16:38 +0200

    add decrypting caesar cipher

--- src/ciphers/caesar/caesar-cipher.spec.ts
+++ src/ciphers/caesar/caesar-cipher.spec.ts
@@ -14,8 +14,8 @@ describe('Caesar Cipher', () => {
         });
 
         describe('when using different key than letter "a"', () => {
-            const openText = 'abcd';
             it('should shift all letters to the right by the number of letters between letter "a" and the key', () => {
+                const openText = 'abcd';
                 expect(englishCaesarCipher.encrypt(openText, 'b')).to.be.equal('bcde');
             });
 
@@ -34,6 +34,59 @@ describe('Caesar Cipher', () => {
                 expect(simpleCaesarCipher.encrypt('123', '4')).to.be.equal('567');
                 expect(simpleCaesarCipher.encrypt('789', '1')).to.be.equal('890');
             });
+        });
+
+        describe('when passing letter that is not from the alphabet', () => {
+            it('should keep the letter as is', () => {
+                expect(englishCaesarCipher.encrypt('Open text!', 'b')).to.be.equal('Oqfo ufyu!');
+            });
+        });
+    });
+
+    describe('when decrypting', () => {
+        describe('when using the letter "a" as a key', () => {
+            it('should always return the same text', () => {
+                const cipherText = 'abcd';
+                expect(englishCaesarCipher.decrypt('a', 'a')).to.be.equal('a');
+                expect(englishCaesarCipher.decrypt(cipherText, 'a')).to.be.equal(cipherText);
+            });
+        });
+
+        describe('when using different key than letter "a"', () => {
+            it('should shift all letters to the left by the number of letters between letter "a" and the key', () => {
+                const cipherText = 'bcde';
+                expect(englishCaesarCipher.decrypt(cipherText, 'b')).to.be.equal('abcd');
+            });
+
+            describe('when shifting over the last letter of alphabet', () => {
+                it('should cycle back to the beginning of the alphabet', () => {
+                    expect(englishCaesarCipher.decrypt('abc', 'b')).to.be.equal('zab');
+                });
+            });
+        });
+
+        describe('when specifying specific alphabet', () => {
+            it('should respect it', () => {
+                const simpleCaesarCipher = new CaesarCipher('0123456789'.split(''));
+                expect(simpleCaesarCipher.decrypt('12345', '0')).to.be.equal('12345');
+                expect(simpleCaesarCipher.decrypt('567', '4')).to.be.equal('123');
+                expect(simpleCaesarCipher.decrypt('890', '1')).to.be.equal('789');
+            });
+        });
+
+        describe('when passing letter that is not from the alphabet', () => {
+            it('should keep the letter as is', () => {
+                expect(englishCaesarCipher.decrypt('Oqfo ufyu!', 'b')).to.be.equal('Open text!');
+            });
+        });
+    });
+
+    describe('integration tests', () => {
+        it('should give the same text when encrypting and decrypting with the same key', () => {
+            const key = 'f';
+            const openText = 'opentext';
+
+            expect(englishCaesarCipher.decrypt(englishCaesarCipher.encrypt(openText, key), key)).to.be.equal(openText);
         });
     });
 });
--- src/ciphers/caesar/caesar-cipher.ts
+++ src/ciphers/caesar/caesar-cipher.ts
@@ -10,12 +10,35 @@ export class CaesarCipher {
         let cipherText = '';
 
         for (const letter of openText) {
-            const cipherIndex = (this.alphabet.findIndex(l => l === letter) + distance);
-            const normalizedCipherIndex = cipherIndex % this.alphabet.length;
-            cipherText += this.alphabet[normalizedCipherIndex];
+            const indexInAlphabet = this.alphabet.findIndex(l => l === letter);
+            if (indexInAlphabet !== -1) {
+                const cipherIndex = (indexInAlphabet + distance);
+                const normalizedCipherIndex = cipherIndex % this.alphabet.length;
+                cipherText += this.alphabet[normalizedCipherIndex];
+            } else {
+                cipherText += letter;
+            }
         }
 
         return cipherText;
+    }
+
+    public decrypt(cipherText: string, key: string): string {
+        const distance = this.findDistance(this.alphabet, key);
+        let openText = '';
+
+        for (const letter of cipherText) {
+            const indexInAlphabet = this.alphabet.findIndex(l => l === letter);
+            if (indexInAlphabet !== -1) {
+                const cipherIndex = (indexInAlphabet - distance) + this.alphabet.length;
+                const normalizedCipherIndex = cipherIndex % this.alphabet.length;
+                openText += this.alphabet[normalizedCipherIndex];
+            } else {
+                openText += letter;
+            }
+        }
+
+        return openText;
     }
 
     private findDistance(alphabet: ReadonlyArray<string>, key: string): number {
