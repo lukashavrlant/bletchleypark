0d396e5ea9f2c5c607b8f726d1aebaef7e655806
Lukáš Havrlant <lukas@havrlant.cz>
2019-05-12 14:39:41 +0200

    export CaesarBruteForceAttack

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export {CaesarBruteForceAttackResults, CaesarBruteForceAttackResult} from "./ciphers/caesar/caesar-brute-force-attack";
+export {CaesarBruteForceAttackResults, CaesarBruteForceAttack} from "./ciphers/caesar/caesar-brute-force-attack";
 export {CaesarCipher} from "./ciphers/caesar/caesar-cipher";
